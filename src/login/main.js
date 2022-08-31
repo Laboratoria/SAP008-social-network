80bf94fa5b8ddf5c51ef2cee28e217fcae450f6b
Ellen Cavalcante <srtnelly@hotmail.com>
2022-08-31 16:39:35 -0300

    Integração do firebase a tela de login

--- src/login/main.js
+++ src/login/main.js
@@ -1,4 +1,23 @@
-// const buttonLoginWithGmail = document.getElementById('button-gmail');
-// const buttonEnter = document.getElementById('button-enter');
-// const inputEmail = document.getElementById('inputEmail');
-// const inputPassword = document.getElementById('inputPassword');
+import { firebaseConfig } from "../firebase/firebase-config.js"
+
+function login() {
+  firebase.auth().signInWithEmailAndPassword(document.getElementById('inputEmail').value, document.getElementById('inputPassword').value).then((user) => {
+    window.location.href = "https://pokemongolive.com/pt_br/";
+  }).catch((error) => {
+    let errorCode = error.code;
+    let errorMessage = error.message;
+    alert(errorMessage)
+  });
+}
+
+function register() {
+  const email = document.getElementById("inputEmail").value;
+  const password = document.getElementById("inputPassword").value;
+  firebase.auth().createUserWithEmailAndPassword(email, password).then(() => {
+    window.location.href = "https://pokemongolive.com/pt_br/";
+  }).catch(error => {
+    alert("Esse e-mail já está cadastrado");
+  });
+}
+document.getElementById("button-enter").addEventListener("click", login);
+document.getElementById("signUp").addEventListener("click", register);

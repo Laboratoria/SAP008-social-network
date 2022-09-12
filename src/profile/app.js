0750a692a552e2898a97f127863332e51042c5b5
Ellen Cavalcante <srtnelly@hotmail.com>
2022-09-12 18:30:34 -0300

    Integração do método currentUser para tratar dados dos usuários e criação do avatar na página profile

--- src/profile/app.js
+++ src/profile/app.js
@@ -1,23 +1,43 @@
 export default () => {
   const container = document.createElement('div');
-  const template = `    <div class="container-profile">
-  <h1>Em construção. 🚧 🏗️</h1>
- 
+  const user = firebase.auth().currentUser;
+  const template = `    <div class="container">
+  <div class="logo">
+      <img id="logo" src="./img/logo.png" alt="logo Vanellen">
+      <span id="idUser"> Olá, ${user.displayName}</span>
+      <span class="VanellenMore">Vanellen <span
+              style="color:rgb(250, 246, 49); font-size:1.5rem; font-weight: bold;">+</span>
+             
   </div>
-  
-  <div class="wrapper"> </div>
-  
-  <footer>
-    <div class="foot">
-        <img src="./img/lab.png" alt="logo Laboratória">
-        <div class="developers">
-            <p> Developed by:</p> 
-            <p>Ellen Cavalcante <spa>&</span> Vanessa Bueck</p>
-        </div>
-    </div>
-  </footer>
-  `;
 
+  <div class="navbar">
+      <a href="./login/index.html"> <img id="logout"  src="./img/logout.png" alt="Ícone de logout"></a>
+      <a href="#profile" id="profile" class="active">Perfil</a>
+      <a href="#newpost">Novo post</a>
+  </div>
+</div>
+
+    <div class="profile-data">
+    <center>
+        <img id="img_redonda" src="${user.photoURL || './img/gif-avatar-vanellen.gif'}" width="450vw"; height="450vh"; alt="A foto do usuário">
+        <h3> Olá, ${user.displayName} </h3>
+        </br>
+        <h2> Senta que lá vem spoiler 🎞️ </h2>
+    </div>
+
+<div class="wrapper"> </div>
+
+<footer>
+  <div class="foot">
+      <img src="./img/lab.png" alt="logo Laboratória">
+      <div class="developers">
+          <p> Developed by:</p> 
+          <p>Ellen Cavalcante <spa>&</span> Vanessa Bueck</p>
+      </div>
+  </div>
+</footer>
+`;
   container.innerHTML = template;
+
   return container;
 };

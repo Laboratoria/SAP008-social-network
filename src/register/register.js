e30812f80d3e6fafd23fb6d7f55e3a42517b1831
Andrea dos Santos <contato.andreadossantos@gmail.com>
2022-09-15 13:53:56 -0300

    link entre a pagina de login e a pagina de cadastro

---
+++ src/register/register.js
@@ -0,0 +1,24 @@
+export default () => {
+    const container = document.createElement('div');
+    const template = `  
+    <div class="container-register">
+
+    <img src="./images/logo_01_marrom_A6634B.png" alt="logo do título"
+        <br>
+        <p class = "sub-title-register">Momentos musicais inesqueciveis</p>                     
+        <form>
+            <input class ="box-name-register" type="name" id="name" placeholder="Nome completo"/>
+            <input class ="box-email-register"type="e-mail" id="e-mail" placeholder="E-mail"/>
+            <input class ="box-password-register" type="password" id="password" placeholder="Digite sua senha"/> 
+            <input class ="box-password-register" id="box-register-password" type="password" id="password" placeholder="Repetir senha"/>   
+            <input class = "btn-register" type="button" id="btn-register" class="btn-register" value="Registrar-se"/>   
+        </form>
+
+        <p class="user"><a href="#login">Já possui uma conta? Iniciar sessão</p>
+
+        </div>     
+        `;
+        
+        container.innerHTML = template;
+        return container;
+}

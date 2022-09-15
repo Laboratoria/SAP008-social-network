b2bdc943ecdf80ab5c3975bb6f0f5bc4a15720cd
fabi basoni <basoni.fabiany@gmail.com>
2022-09-15 14:52:07 -0300

    switch funcionando, botao de retornar da login page funcionando

--- src/pages/login/main.js
+++ src/pages/login/main.js
@@ -2,24 +2,25 @@ export default () => {
   const loginContainer = document.createElement('div');
   const template = `
           <header class="header-login display-flex">
-              <a href="/#homepage" class="return-btn"><img class="return-btn" src="img/returnBtn.png" alt="Return button"></a>
+              <a href="/#homepage" class="return-btn" id="return-btn"><img class="return-btn" src="img/returnBtn.png" alt="back arrow"></a>
               <h1><img class="img-logo-login" src="img/Rebu.png" alt="Rebu Logo"></h1>
           </header>
           <main id="login-page" class="login-page display-flex">
             <form class="login-form display-flex">
                 <h2 class="login-text">ENTRAR</h2>
-                <input type="text" placeholder="  E-MAIL" id="email-input-login" class="input-text-login"><p>
-                <input type="text" placeholder="  SENHA" id="password-input-login" class="input-text-login"></p>
-                <a href="/#resetpassword" class="link-text">ESQUECEU SUA SENHA? CLIQUE AQUI</a>
+                <input type="email" placeholder="E-MAIL" id="email-input-login" class="input-text-login"><p>
+                <input type="password" placeholder="SENHA" id="password-input-login" class="input-text-login"></p>
+                <a href="/#resetpassword" class="password-reset-login">ESQUECEU SUA SENHA? CLIQUE AQUI</a>
                 <a href="/#feed"><button type="button" id="btn-login-page" class="btn-login">ENTRAR</button></a>
-                <legend class="google-login-legend">OU
-                <p>ENTRE COM O GOOGLE</p>
-                </legend>
-                <a href="/#feed"><img class="google-btn" src="img/googleIcon.png" alt="Google button"></a>
-                <a href="/#register" class="link-text">NÃO POSSUI UMA CONTA? CADASTRE-SE!</a>
             </form class="login-form">
+            <button class="btn-google-login display-flex"><img class="google-btn-register" src="img/googleIcon.png" alt="google logo">ENTRE COM O GOOGLE</button>
+            <a href="/#register" class="link-text-login">NÃO POSSUI UMA CONTA? CADASTRE-SE!</a>
           </main>
       `;
   loginContainer.innerHTML = template;
+
+  const returnBtn = loginContainer.querySelector('#return-btn');
+  returnBtn.addEventListener('click', () => window.location.replace('#homepage'));
+
   return loginContainer;
 };

1825691077612668c62a88ecd9871c4f59417f00
fabi basoni <basoni.fabiany@gmail.com>
2022-09-16 10:30:02 -0300

    adiciona fonte blanka ao about

--- src/firebase/firebase-config.js
+++ src/firebase/firebase-config.js
@@ -1,12 +1,12 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.3/firebase-app.js";
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.9.3/firebase-app.js';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCeyHG_CqM2GbG5cF1QNHb4seKQoJ4uJDA",
-  authDomain: "rebu-social-network.firebaseapp.com",
-  projectId: "rebu-social-network",
-  storageBucket: "rebu-social-network.appspot.com",
-  messagingSenderId: "988363110012",
-  appId: "1:988363110012:web:43365a6efad711699bfbf9"
+  apiKey: 'AIzaSyCeyHG_CqM2GbG5cF1QNHb4seKQoJ4uJDA',
+  authDomain: 'rebu-social-network.firebaseapp.com',
+  projectId: 'rebu-social-network',
+  storageBucket: 'rebu-social-network.appspot.com',
+  messagingSenderId: '988363110012',
+  appId: '1:988363110012:web:43365a6efad711699bfbf9',
 };
 
 const app = initializeApp(firebaseConfig);
--- src/pages/about/main.js
+++ src/pages/about/main.js
@@ -5,10 +5,10 @@ export default () => {
       <a href="/#homepage" class="return-btn"><img class="return-btn" src="img/returnBtn.png" alt="back arrow"></a>
     </header>
     <main id="about-page">
-      <h2 id="about-title">SOBRE <strong class="about-rebu">REBU</strong></h2>
-      <p class="about-paragraph"><strong class="about-rebu">REBU</strong> é uma rede social de interação & comunidade feita para mulheres lésbicas.</p> 
+      <h2 id="about-title">SOBRE <span class="about-rebu-title">REBU</span></h2>
+      <p class="about-paragraph"><span class="about-rebu">REBU</span> é uma rede social de interação & comunidade feita para mulheres lésbicas.</p> 
       <p class="about-paragraph">Proporcionamos um espaço inclusivo e seguro para nossas usuárias serem suas versões mais autênticas, encontrarem novas amizades ou relacionamentos a partir de interesses em comum, se conectarem.</p> 
-      <p class="about-paragraph">Não importa onde você more, <strong class="about-rebu">REBU</strong> te prorporciona se conectar com outras mulheres e criar sua própria rede.</p>
+      <p class="about-paragraph">Não importa onde você more, <span class="about-rebu">REBU</span> te prorporciona se conectar com outras mulheres e criar sua própria rede.</p>
     </main>
   `;
   aboutContainer.innerHTML = template;

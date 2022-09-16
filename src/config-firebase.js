2a3394302507ad7eb1901e47901a3b30226c9d9b
Bellalimalima <isabellalimaelima@gmail.com>
2022-09-15 21:05:47 -0300

    instalando firebase

---
+++ src/config-firebase.js
@@ -0,0 +1,14 @@
+
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.9.3/firebase-app.js';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC7EJz1DjW_J3P-zh4EEUKhO2iohn7t_as",
+  authDomain: "picsfem-31f94.firebaseapp.com",
+  projectId: "picsfem-31f94",
+  storageBucket: "picsfem-31f94.appspot.com",
+  messagingSenderId: "1073640265761",
+  appId: "1:1073640265761:web:c3781fc04688a3560c2b60"
+};
+
+
+const app = initializeApp(firebaseConfig);

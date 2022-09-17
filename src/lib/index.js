2d70d59bc71be4b94b8ec143f7e6bf09810f82f7
fabi basoni <basoni.fabiany@gmail.com>
2022-09-17 11:22:24 -0300

    conserta modules de configuracao e funcoes do firebase

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,6 +1,39 @@
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from 'https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js';
+// import { db } from '... firestore.js'
+
+import { app } from './firebase-configuration.js';
+
+export function registerWithEmailAndPassword(email, password) {
+  const auth = getAuth(app);
+  createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      const user = userCredential.user;
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+    });
+}
+
+export function loginWithEmailAndPassword(email, password) {
+  const auth = getAuth(app);
+  signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      const user = userCredential.user;
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+    });
+}
+
 // aqui exportaras las funciones que necesites
 
-export const myFunction = () => {
-  // aqui tu codigo
-  console.log('Hola mundo!');
-};
+// export const myFunction = () => {
+//   // aqui tu codigo
+//   console.log('Hola mundo!');
+// };
--- src/pages/login/main.js
+++ src/pages/login/main.js
@@ -5,16 +5,23 @@ export default () => {
               <a href="/#homepage" class="return-btn" id="return-btn"><img class="return-btn" src="img/returnBtn.png" alt="back arrow"></a>
               <h1><img class="img-logo-login" src="img/Rebu.png" alt="Rebu Logo"></h1>
           </header>
+
           <main id="login-page" class="login-page display-flex">
+
             <form class="login-form display-flex">
+
                 <h2 class="login-text">ENTRAR</h2>
                 <input type="email" placeholder="E-MAIL" id="email-input-login" class="input-text-login"><p>
                 <input type="password" placeholder="SENHA" id="password-input-login" class="input-text-login"></p>
                 <a href="/#resetpassword" class="password-reset-login">ESQUECEU SUA SENHA? CLIQUE AQUI</a>
                 <a href="/#feed"><button type="button" id="btn-login-page" class="btn-login">ENTRAR</button></a>
+
             </form class="login-form">
+
             <button class="btn-google-login display-flex"><img class="google-btn-register" src="img/googleIcon.png" alt="google logo">ENTRE COM O GOOGLE</button>
+            
             <a href="/#register" class="link-text-login">NÃO POSSUI UMA CONTA? CADASTRE-SE!</a>
+
           </main>
       `;
   loginContainer.innerHTML = template;

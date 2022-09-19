a23624a486584782f73705b99b850c29854a6b4d
Bellalimalima <isabellalimaelima@gmail.com>
2022-09-19 14:20:45 -0300

    função de login

--- src/config-firebase.js
+++ src/config-firebase.js
@@ -1,14 +1,66 @@
 
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.9.3/firebase-app.js';
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut } from 'https://www.gstatic.com/firebasejs/9.9.3/firebase-auth.js'
+import { btnLogin, btnSignup, btnLogout, txtEmail, txtPassword } from './main.js';
+
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC7EJz1DjW_J3P-zh4EEUKhO2iohn7t_as",
-  authDomain: "picsfem-31f94.firebaseapp.com",
-  projectId: "picsfem-31f94",
-  storageBucket: "picsfem-31f94.appspot.com",
-  messagingSenderId: "1073640265761",
-  appId: "1:1073640265761:web:c3781fc04688a3560c2b60"
+  apiKey: "AIzaSyC0LYibd7iDrRSG9t2pNqWjxVVpcZxuyuo",
+  authDomain: "teste-689a5.firebaseapp.com",
+  projectId: "teste-689a5",
+  storageBucket: "teste-689a5.appspot.com",
+  messagingSenderId: "584087173369",
+  appId: "1:584087173369:web:d5c821c45f15ff5cde0a28"
 };
 
-
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+const creatAccount = async () => {
+  const loginEmail = txtEmail.value;
+  const loginPassword = txtPassword.value;
+
+  try {
+    const userCredential = await createUserWithEmailAndPassword(auth, loginEmail, loginPassword);
+    console.log(userCredential.user);
+  }
+  catch (error) {
+    console.log(error)
+  }
+}
+
+btnSignup.addEventListener("click", creatAccount);
+
+
+const loginEmailPassword = async () => {
+  const loginEmail = txtEmail.value;
+  const loginPassword = txtPassword.value;
+
+  try {
+    const userCredential = await signInWithEmailAndPassword(auth, loginEmail, loginPassword);
+    console.log(userCredential.user);
+  }
+  catch (error) {
+    console.log(error);
+  }
+}
+
+btnLogin.addEventListener("click", loginEmailPassword);
+
+
+
+onAuthStateChanged(auth, user => {
+  if (user != null) {
+    console.log('logged in!');
+  } else {
+    console.log('No user');
+  }
+});
+
+
+const logout = async () => {
+  await signOut(auth);
+}
+
+btnLogout.addEventListener("click", logout)
+

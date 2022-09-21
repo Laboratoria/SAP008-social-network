a3b5159ef4cd93cbc0695da04fd086d84373d9f6
nunesisabela <isabelanunes.tech@gmail.com>
2022-09-21 16:06:53 -0300

    firebase configurado em novo arquivo importado para main

---
+++ src/firebase-config.js
@@ -0,0 +1,12 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.9.4/firebase-app.js';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyB2o3fpqujNIeH6m0-YzEgbEoc9xFieVDk",
+    authDomain: "bateprato-social-network.firebaseapp.com",
+    projectId: "bateprato-social-network",
+    storageBucket: "bateprato-social-network.appspot.com",
+    messagingSenderId: "414001246820",
+    appId: "1:414001246820:web:f176e5eb081d19f0ac6571"
+  };
+  
+  const app = initializeApp(firebaseConfig);  

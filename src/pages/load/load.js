65ec5efa7f95927d0d226ad6beb8f27e5520e5a6
nunesisabela <isabelanunes.tech@gmail.com>
2022-09-22 15:43:50 -0300

    template que carrega com evento load

---
+++ src/pages/load/load.js
@@ -0,0 +1,25 @@
+const template = 
+`<section class="container">
+    <div class="frame">
+      <h1 class="titles">Cadastre-se<br>no BatePrato</h1>
+
+      <button id="google-login">Entrar com Google</button>
+
+      <div class="lines">
+        <hr>
+        <p>ou</p>
+        <hr>
+      </div>
+
+      <button id="email-signin">Inscreva-se com e-mail</button>
+
+      <p class="instructions">Ao se inscrever, você concorda com os
+        <a class="cta" href="">Termos<br>de Serviço</a> e a
+        <a class="cta" href="">Política de Privacidade</a>, incluindo<br>o
+        <a class="cta" href="">Uso de Cookies.</a>
+      </p>
+
+      <p class="instructions">Já tem uma conta? <a href="/#entrar" class="cta">Entrar</a></p>
+    </div>
+    <div class="logo"></div>
+  </section>`

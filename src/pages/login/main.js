06b4d66b3ae162a826ddc58725509a143ef2ed8f
Clarissa Simplício <clasimplicio@hotmail.com>
2022-09-22 15:13:52 -0300

    ajeitando erros de ESLint

--- src/pages/login/main.js
+++ src/pages/login/main.js
@@ -1,4 +1,3 @@
-
 import {
   loginWithEmailAndPassword,
   loginWithGoogle,
@@ -7,31 +6,31 @@ import {
 export default () => {
   const loginContainer = document.createElement('div');
   const template = `
-          <header class="header-login display-flex">
-              <a href="/#homepage" class="return-btn" id="return-btn"><img class="return-btn" src="img/returnBtn.png" alt="back arrow"></a>
-              <h1><img class="img-logo-login" src="img/Rebu.png" alt="Rebu Logo"></h1>
-          </header>
+    <header class="header-login display-flex">
+        <a href="/#homepage" class="return-btn" id="return-btn"><img class="return-btn" src="img/returnBtn.png" alt="back arrow"></a>
+        <h1><img class="img-logo-login" src="img/Rebu.png" alt="Rebu Logo"></h1>
+    </header>
 
-          <main id="login-page" class="login-page display-flex">
+    <main id="login-page" class="login-page display-flex">
 
-            <form class="login-form display-flex">
+      <form class="login-form display-flex">
 
-              <h2 class="login-text">ENTRAR</h2>
-              <input type="email" placeholder="E-MAIL" id="email-input-login" class="input-text-login"><p>
-              <input type="password" placeholder="SENHA" id="password-input-login" class="input-text-login"></p>
-              
-            </form>
+        <h2 class="login-text">ENTRAR</h2>
+        <input type="email" placeholder="E-MAIL" id="email-input-login" class="input-text-login"><p>
+        <input type="password" placeholder="SENHA" id="password-input-login" class="input-text-login"></p>
 
-            <a href="/#resetPassword" class="password-reset-login">ESQUECEU SUA SENHA? CLIQUE AQUI</a>
-                
-            <a href="/#feed"><button type="button" id="btn-login-page" class="btn-login">ENTRAR</button></a>
-            
-            <button class="btn-google-login display-flex" id="google-btn"><img class="google-icon" src="img/googleIcon.png" alt="google logo">ENTRE COM O GOOGLE</button>
+      </form>
 
-            <a href="/#register" class="link-text-login">NÃO POSSUI UMA CONTA? CADASTRE-SE!</a>
+      <a href="/#resetPassword" class="password-reset-login">ESQUECEU SUA SENHA? CLIQUE AQUI</a>
 
-          </main>
-      `;
+      <a href="/#feed"><button type="button" id="btn-login-page" class="btn-login">ENTRAR</button></a>
+
+      <button class="btn-google-login display-flex" id="google-btn"><img class="google-icon" src="img/googleIcon.png" alt="google logo">ENTRE COM O GOOGLE</button>
+
+      <a href="/#register" class="link-text-login">NÃO POSSUI UMA CONTA? CADASTRE-SE!</a>
+
+    </main>
+  `;
   loginContainer.innerHTML = template;
 
   const returnBtn = loginContainer.querySelector('#return-btn');
@@ -40,8 +39,8 @@ export default () => {
   const inputEmail = loginContainer.querySelector('#email-input-login');
   const inputPasssword = loginContainer.querySelector('#password-input-login');
   const btnLogin = loginContainer.querySelector('#btn-login-page');
-  const btnResetPassword = loginContainer.querySelector('.password-reset-login');
-    
+  // const btnResetPassword = loginContainer.querySelector('.password-reset-login');
+
   btnLogin.addEventListener('click', () => {
     loginWithEmailAndPassword(inputEmail.value, inputPasssword.value)
       .then(() => {
@@ -53,7 +52,7 @@ export default () => {
         const errorMessage = error.message; */
       });
   });
-  
+
   const googleBtn = loginContainer.querySelector('#google-btn');
 
   googleBtn.addEventListener('click', () => {
--- src/pages/resetPassword/main.js
+++ src/pages/resetPassword/main.js
@@ -1,30 +1,30 @@
 import {
-    resetPassword 
-  } from "../../lib/index.js";
+  resetPassword,
+} from '../../lib/index.js';
 
 export default () => {
   const resetContainer = document.createElement('div');
   const template = `
-          <header class="header-login display-flex">
-              <a href="/#homepage" class="return-btn" id="return-btn"><img class="return-btn" src="img/returnBtn.png" alt="back arrow"></a>
-              <h1><img class="img-logo-login" src="img/Rebu.png" alt="Rebu Logo"></h1>
-          </header>
-  
-          <main id="reset-page" class="reset-page display-flex">
-  
-            <form class="reset-form display-flex">
-  
-              <h2 class="reset-text">REDEFINA SUA SENHA</h2>
-              <p>ENVIAREMOS UM E-MAIL PARA REDEFINIÇÃO DE SENHA</p>
-              <input type="email" placeholder="E-MAIL" id="email-input-reset" class="input-text-reset"><p>
-                
-            </form>
-                  
-            <a href="/#homepage"><button type="button" id="btn-reset-page" class="btn-reset">ENVIAR</button></a>
-  
-          </main>
-        `;
-    
+    <header class="header-login display-flex">
+        <a href="/#homepage" class="return-btn" id="return-btn"><img class="return-btn" src="img/returnBtn.png" alt="back arrow"></a>
+        <h1><img class="img-logo-login" src="img/Rebu.png" alt="Rebu Logo"></h1>
+    </header>
+
+    <main id="reset-page" class="reset-page display-flex">
+
+      <form class="reset-form display-flex">
+
+        <h2 class="reset-text">REDEFINA SUA SENHA</h2>
+        <p>ENVIAREMOS UM E-MAIL PARA REDEFINIÇÃO DE SENHA</p>
+        <input type="email" placeholder="E-MAIL" id="email-input-reset" class="input-text-reset"><p>
+
+      </form>
+
+      <a href="/#homepage"><button type="button" id="btn-reset-page" class="btn-reset">ENVIAR</button></a>
+
+    </main>
+  `;
+
   resetContainer.innerHTML = template;
 
   const returnBtn = resetContainer.querySelector('#return-btn');
@@ -32,18 +32,17 @@ export default () => {
   const sendBtn = resetContainer.querySelector('#btn-reset-page');
 
   returnBtn.addEventListener('click', () => window.location.replace('#homepage'));
-  
+
   sendBtn.addEventListener('click', () => {
     resetPassword(emailValue)
       .then(() => {
-        alert('Email enviado')
+        alert('Email enviado');
       })
-      .catch((error) => {
-       const errorCode = error.code;
-        const errorMessage = error.message;
-      })
-  })
+      .catch((/* error */) => {
+      /*  const errorCode = error.code;
+        const errorMessage = error.message; */
+      });
+  });
 
   return resetContainer;
-}
-
+};

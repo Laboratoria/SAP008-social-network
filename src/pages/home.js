77989312c590af5a419b677290c1e2450e746acc
as_mih <michelledearaujo2@gmail.com>
2022-09-22 16:16:37 -0300

    pagina inicial sem funcionar e config firebase
    Co-authored-by: Ray Pinto <rayannepinto@users.noreply.github.com>
    Co-authored-by: Alana Karine <AlanaK2@users.noreply.github.com>

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,15 +1,9 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <title>Booknotes</title>
-  </head>
-  <body>
-    <div id="app"></div>
-    <!-- isso vai pra dentro do js-->
-    <section class="login">
+// Este es el punto de entrada de tu aplicacion
+export const myFunction = () => {
+  const container = document.createElement("div");
+
+  const templateLogin = `
+<section class="login">
       <div>
         <img src="./img/logo.png" alt="Logo Booknotes" />
         <p>
@@ -25,7 +19,8 @@
         <button type="submit">Cadastre-se!</button>
       </div>
     </section>
-    <!---->
-    <script type="module" src="main.js"></script>
-  </body>
-</html>
+`;
+  container.innerHTML = templateLogin;
+  return container;
+  myFunction();
+};

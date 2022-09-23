f2d654b2a7b742de5fedb5b983e790d8d511eb8e
as_mih <michelledearaujo2@gmail.com>
2022-09-23 19:44:53 -0300

    resolução de conflitos estilizacao login Co-authored-by: Ray Pinto <rayannepinto@users.noreply.github.com> Co-authored-by: Alana Karine <AlanaK2@users.noreply.github.com>

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,20 +3,24 @@ export const homeFunction = () => {
   const container = document.createElement('div');
 
   const templateLogin = `
-<section class="login">
-      <div>
-        <img src="./img/logo.png" alt="Logo Booknotes" />
-        <p>
+<section class="home-page">
+      <div class="background-home"> 
+        <p class="background-home quote">
           “Sempre fomos o que os homens disseram que nós éramos. Agora somos nós
           que vamos dizer o que somos.“ — Lygia Fagundes Telles
         </p>
-        <p>Faça seu login!</p>
-        <input type="email" placeholder="leitora@email.com" />
-        <input type="text" placeholder="***********" />
-        <button type="submit">Entrar</button>
-        <button type="submit">Entrar com o google</button>
-        <p>Ainda não tem uma conta?</p>
-        <button type="submit">Cadastre-se!</button>
+        <img class="img-woman" src="../src/img/reading.png" alt="Mulher sentada e lendo livro">
+      </div>   
+      <div class="form">
+        <picture><img src="/src/img/logo.png" class="img-logo" alt="Logo Booknotes"></picture>
+        <p class="text-login text-one">Faça seu login!</p>
+        <input class="input-login" type="email" placeholder="leitora@email.com" />
+        <input class="input-login" type="text" placeholder="***********" />
+        <button class="button-sign-in button-login" type="submit">Entrar</button>
+        <p class="text-login text-two">────── Ou entrar com ──────</p>
+        <a href=""><img src="/src/img/google.png" class="button-google" alt="Logo do Google"></a>
+        <p class="text-login text-three">Ainda não tem uma conta?</p>
+        <button class="button-sign-up button-login" type="submit">Cadastre-se!</button>
       </div>
     </section>
 `;

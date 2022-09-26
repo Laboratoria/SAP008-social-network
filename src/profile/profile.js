dc660aa7328cefce7033aba90785fed5f9b5753f
AngelMelo12 <angellmello1993@gmail.com>
2022-09-26 11:50:08 -0300

    adição de containers para facilitar estilização no css

--- src/profile/profile.js
+++ src/profile/profile.js
@@ -1,13 +1,21 @@
 export default () => {
     const container = document.createElement('div');
     const template = `  
-        <div class="container-profile">
-            <img src="./images/logo_02_azul_081E26.png" alt="logo do título"
-            <h1>Meu perfil musical</h1>   
-
-
-            
-               </div>     
+    <div class="container-logo-profile">
+        <img class="logo-img-profile" src="./images/logo_02_azul_081E26.png" alt="logo do título">
+    </div>
+    <div class="container-title-profile">
+        <h1>Meu perfil musical</h1>
+    </div>    
+    <div class="container-profile">
+        <form>
+            <input type="text" id="name" placeholder="Nome"/>
+            <input type="text" id="age" placeholder="Idade"/>
+            <p class="about" id="about"> Sobre mim</p>
+            <textarea class="box-description" id="description" placeholder="Nos fale um pouco sobre você"></textarea> 
+            <button type="button" id="btn-create-post" class="btn-create-post">Crie um post</button>
+            </form>
+        </div>     
     `;
 
     container.innerHTML = template;

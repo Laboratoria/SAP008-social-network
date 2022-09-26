212b7b735dacd68e4ab516d5ebac274383725b12
as_mih <michelledearaujo2@gmail.com>
2022-09-26 19:22:17 -0300

    corrigindo css da pagina de login
    Co-authored-by: Ray Pinto <rayannepinto@users.noreply.github.com>
    Co-authored-by: Alana Karine <AlanaK2@users.noreply.github.com>

--- src/pages/home.js
+++ src/pages/home.js
@@ -9,16 +9,16 @@ export const homeFunction = () => {
           “Sempre fomos o que os homens disseram que nós éramos. Agora somos nós
           que vamos dizer o que somos.“ — Lygia Fagundes Telles
         </p>
-        <img class="img-woman" src="../src/img/reading.png" alt="Mulher sentada e lendo livro">
+        <img class="img-woman" src="img\\reading.png" alt="Mulher sentada e lendo livro">
       </div>   
       <div class="form">
-        <picture><img src="/src/img/logo.png" class="img-logo" alt="Logo Booknotes"></picture>
+        <picture><img src="img\\logo.png" class="img-logo" alt="Logo Booknotes"></picture>
         <p class="text-login text-one">Faça seu login!</p>
         <input class="input-login" type="email" placeholder="leitora@email.com" />
         <input class="input-login" type="text" placeholder="***********" />
         <button class="button-sign-in button-login" type="submit">Entrar</button>
-        <p class="text-login text-two">────── Ou entrar com ──────</p>
-        <a href=""><img src="/src/img/google.png" class="button-google" alt="Logo do Google"></a>
+        <p class="text-login text-two">───── Ou entrar com ─────</p>
+        <a href=""><img src="img\\google.png" class="button-google" alt="Logo do Google"></a>
         <p class="text-login text-three">Ainda não tem uma conta?</p>
         <button class="button-sign-up button-login" type="submit">Cadastre-se!</button>
       </div>

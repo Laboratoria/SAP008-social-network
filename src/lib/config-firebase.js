d60f5cab05627e34f64b9d8d996bb47996e0223d
AghathaK <aghatha.kriss01@gmail.com>
2022-09-26 18:19:53 -0300

    criação do HTML e CSS

---
+++ src/lib/config-firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.4/firebase-app.js";
+const firebaseConfig = {
+  apiKey: "AIzaSyDHGk3ieHViluVOH5JSgX9Pfqn_jyzvQig",
+  authDomain: "social-networking-lab.firebaseapp.com",
+  projectId: "social-networking-lab",
+  storageBucket: "social-networking-lab.appspot.com",
+  messagingSenderId: "7195647788",
+  appId: "1:7195647788:web:97deb70de0cbb23aac9689",
+  measurementId: "G-3D1BPNVQ1E"
+};
+
+const app = initializeApp(firebaseConfig);

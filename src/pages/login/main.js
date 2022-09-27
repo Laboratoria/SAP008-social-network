76e6242b2b53aa88da517a4e04bf602838a661d8
joyce <joyce.mbt@gmail.com>
2022-09-27 11:59:53 -0300

    consertando os erros de estilização

--- src/pages/login/main.js
+++ src/pages/login/main.js
@@ -11,47 +11,33 @@ export default () => {
         <h1><img class="img-logo-login" src="img/Rebu.png" alt="Rebu Logo"></h1>
     </header>
 
-    <main id="login-page" class="login-page display-flex">
+    <div class="gif-side-desktop" id="desktop-page">
+        <video autoplay loop class="bg-gif-desktop">
+          <source src="img/gifDesktop.mp4" type="video/mp4">
+        </video>
+        <img src="img/Rebu.png" alt="Rebu Logo" class="rebu-logo-desktop">
+    </div>
 
+    
+    <main id="login-page" class="login-page display-flex">
+    
+      <h1 class="text-desktop">CONECTE-SE</h1>
       <form class="login-form display-flex">
 
         <h2 class="login-text">ENTRAR</h2>
         <input type="email" placeholder="E-MAIL" id="email-input-login" class="input-text-login">
         <input type="password" placeholder="SENHA" id="password-input-login" class="input-text-login">
 
+        <a href="/#resetPassword" class="password-reset-login">ESQUECEU SUA SENHA? CLIQUE AQUI</a>
+  
+        <a href="/#feed"><button type="button" id="btn-login-page" class="btn-login">ENTRAR</button></a>
+  
+        <button class="btn-google-login display-flex" id="google-btn"><img class="google-icon" src="img/googleIcon.png" alt="google logo">ENTRE COM O GOOGLE</button>
+  
+        <a href="/#register" class="link-text-login">NÃO POSSUI UMA CONTA? CADASTRE-SE!</a>
       </form>
 
-      <a href="/#resetPassword" class="password-reset-login">ESQUECEU SUA SENHA? CLIQUE AQUI</a>
-
-      <a href="/#feed"><button type="button" id="btn-login-page" class="btn-login">ENTRAR</button></a>
-
-      <button class="btn-google-login display-flex" id="google-btn"><img class="google-icon" src="img/googleIcon.png" alt="google logo">ENTRE COM O GOOGLE</button>
-
-      <a href="/#register" class="link-text-login">NÃO POSSUI UMA CONTA? CADASTRE-SE!</a>
     </main>
-
-    <div class="desktop-login">
-      <div class="gif-side-desktop" id="desktop-page">
-        <video autoplay loop class="bg-gif-desktop">
-          <source src="img/gifDesktop.mp4" type="video/mp4">
-        </video>
-        <img src="img/Rebu.png" alt="Rebu Logo" class="rebu-logo">
-      </div>
-      <main class="login-side-desktop">
-        <h1 class="text-desktop">CONECTE-SE</h1>
-        <form class="desktop-form">
-          <input type="email" placeholder="E-MAIL" id="email-input-login" class="input-desktop">
-          <input type="password" placeholder="SENHA" id="password-input-login" class="input-desktop">
-          <a href="/#resetPassword" class="password-reset-login">ESQUECEU SUA SENHA? CLIQUE AQUI</a>
-  
-          <a href="/#feed"><button type="button" id="btn-login-page" class="btn-login-desktop btn-login">ENTRAR</button></a>
-  
-          <button class="btn-google-desktop display-flex" id="google-btn"><img class="google-icon" src="img/googleIcon.png" alt="google logo">ENTRE COM O GOOGLE</button>
-  
-          <a href="/#register" class="register-desktop">NÃO POSSUI UMA CONTA? CADASTRE-SE!</a>
-        </form>
-      </main>
-  </div>
   `;
   loginContainer.innerHTML = template;
 

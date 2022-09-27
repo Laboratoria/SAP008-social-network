c835b8d00a327c7906ee52673a32f320cbe9a244
joyce <joyce.mbt@gmail.com>
2022-09-27 17:44:35 -0300

    consertando parte da estilização da pagina reset password

--- src/pages/resetPassword/main.js
+++ src/pages/resetPassword/main.js
@@ -5,22 +5,27 @@ import {
 export default () => {
   const resetContainer = document.createElement('div');
   const template = `
-          <header class="header-login display-flex">
+          <header class="header-reset display-flex">
               <a href="/#homepage" class="return-btn" id="return-btn"><img class="return-btn" src="img/returnBtn.png" alt="back arrow"></a>
               <h1><img class="img-logo-login" src="img/Rebu.png" alt="Rebu Logo"></h1>
           </header>
+          <div class="gif-side-desktop" id="desktop-page">
+          <video autoplay loop class="bg-gif-desktop">
+            <source src="img/gifDesktop.mp4" type="video/mp4">
+          </video>
+          <img src="img/Rebu.png" alt="Rebu Logo" class="rebu-logo-desktop">
+          </div>
   
-          <main id="reset-page" class="reset-page display-flex">
-  
+          <main id="reset-page" class="reset-page">
+          
             <form class="reset-form display-flex">
-  
               <h2 class="reset-text">REDEFINA SUA SENHA</h2>
               <p class="message-about-reset">ENVIAREMOS UM E-MAIL PARA REDEFINIÇÃO DE SENHA</p>
-              <input type="email" placeholder="E-MAIL" id="email-input-reset" class="input-style"><p>
-                
+              <input type="email" placeholder="E-MAIL" id="email-input-reset" class="input-style-reset"><p>
+              <a href="/#homepage"><button type="button" id="btn-cancel-reset" class="btn-cancel-reset">CANCELAR</button></a>
+              <a href="/#homepage"><button type="button" id="btn-reset-page" class="btn-reset">ENVIAR</button></a>
             </form>
                   
-            <a href="/#homepage"><button type="button" id="btn-reset-page" class="btn-reset">ENVIAR</button></a>
   
           </main>
         `;
@@ -31,7 +36,8 @@ export default () => {
   const sendBtn = resetContainer.querySelector('#btn-reset-page');
 
   returnBtn.addEventListener('click', () => window.location.replace('#homepage'));
-  sendBtn.addEventListener('click', () => {
+  sendBtn.addEventListener('click', (e) => {
+    e.preventDefault();
     resetPassword(emailValue.value)
       .then(() => {
         alert('Email enviado');

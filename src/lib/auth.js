09d3a7579bd78de4b49d98a8cb19101b505e2064
Amanda Holanda <amandaholanda_@hotmail.com>
2022-09-27 13:48:47 -0300

    erros eslint

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,63 +1,70 @@
 import { app } from './firebase.js';
 
-import { getAuth, signInWithEmailAndPassword, onAuthStateChanged, createUserWithEmailAndPassword, signOut, GoogleAuthProvider, signInWithPopup }
-from './export.js';
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+  createUserWithEmailAndPassword,
+  signOut,
+  GoogleAuthProvider,
+  signInWithPopup,
+} from './export.js';
 
 const auth = getAuth(app);
 
-onAuthStateChanged(auth, user => {
-    console.log(user)
-    if (user != null) {
-        console.log('logged in!');
-    } else {
-        console.log('No user');
-    }
+onAuthStateChanged(auth, (user) => {
+  console.log(user);
+  if (user != null) {
+    console.log('logged in!');
+  } else {
+    console.log('No user');
+  }
 });
 
 const loginEmailPassword = async (email, password) => {
-
-    try {
-        const userCredential = await signInWithEmailAndPassword(auth, email, password);
-        console.log(userCredential);
-    }
-    catch (error) {
-        console.log(error);
-    }
-}
+  try {
+    const userCredential = await signInWithEmailAndPassword(auth, email, password);
+    console.log(userCredential);
+  }
+  catch (error) {
+    console.log(error);
+  }
+};
 
 const createAccount = async (email, password) => {
-
-    try {
-        const userCredential = await createUserWithEmailAndPassword(auth, email, password);
-        console.log(userCredential.user);
-
-    }
-    catch (error) {
-        console.log(error)
-    }
-}
+  try {
+    const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+    console.log(userCredential.user);
+  }
+  catch (error) {
+    console.log(error);
+  }
+};
 
 const logout = async () => {
-    await signOut(auth);
-}
+  await signOut(auth);
+};
 
 const provider = new GoogleAuthProvider;
 const signInGoogle = () => {
-    return signInWithPopup(auth, provider)
-        .then((result) => {
-            const credential = GoogleAuthProvider.credentialFromResult(result);
-            const token = credential.accessToken;
-            const user = result.user;
+  return signInWithPopup(auth, provider)
+    .then((result) => {
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const token = credential.accessToken;
+      const user = result.user;
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      const email = error.customData.email;
+      const credential = GoogleAuthProvider.credentialFromError(error);
+    });
+};
 
-        }).catch((error) => {
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            const email = error.customData.email;
-            const credential = GoogleAuthProvider.credentialFromError(error);
-        });
-}
-
-
-export { auth, loginEmailPassword, createAccount, logout, signInGoogle };
-
-
+export {
+  auth,
+  loginEmailPassword,
+  createAccount,
+  logout,
+  signInGoogle,
+};
--- src/lib/export.js
+++ src/lib/export.js
@@ -1,9 +1,11 @@
-export { getAuth, 
-    signInWithEmailAndPassword, 
-    onAuthStateChanged, 
-    createUserWithEmailAndPassword, 
-    signOut, 
-    GoogleAuthProvider, 
-    signInWithPopup } from 'https://www.gstatic.com/firebasejs/9.9.3/firebase-auth.js';
+export {
+  getAuth,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+  createUserWithEmailAndPassword,
+  signOut,
+  GoogleAuthProvider,
+  signInWithPopup,
+} from 'https://www.gstatic.com/firebasejs/9.9.3/firebase-auth.js';
 
 export { initializeApp } from 'https://www.gstatic.com/firebasejs/9.9.3/firebase-app.js';

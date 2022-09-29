d772687b5041176e24594d99629307620d7c6dc3
andresavieira28 <andresacg_14@hotmail.com>
2022-09-29 17:48:23 -0300

    refresh box model

--- src/pages/password/password.js
+++ src/pages/password/password.js
@@ -1,14 +1,16 @@
 export default () => {
   const sectionPassword = document.createElement('section');
   const content = `
-    <section class='boxModel'>
-      <form>
-        <h2 class="titleSenha">Vamos recuperar sua senha!</h2>
-        <input class="recuperarSenha" type="text" id="username" placeholder="E-mail"  value="">
-        <div class="">  Ops, confira seus dados! </div>
-        <button class="btnContinuar" type="submit" >CONTINUAR</button>
-      </form>
-    </section>`;
+    <body class="bodyPassword">
+      <section class='boxModel'>
+        <form class="formPassword">
+          <h2 class="titleSenha">Vamos recuperar sua senha!</h2>
+          <input class="recuperarSenha" type="text" id="username" placeholder="E-mail"  value="">
+          <div class="">  Ops, confira seus dados! </div>
+          <button class="btnContinuar" type="submit" >CONTINUAR</button>
+        </form>
+      </section>
+    <body>`;
   sectionPassword.innerHTML = content;
   return sectionPassword;
 };

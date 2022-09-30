87970b52a69fe4277fad3f7108191cb94ace8295
adevcarol <alvscarolina@outlook.com>
2022-09-30 13:38:21 -0300

    Posicionamento da logo e título, estilzação inicial da tela de login

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -2,9 +2,24 @@ export default () => {
     const container = document.createElement('div'); 
 
     const template = `
-        <h1> Login </h1>
-        <a href= "#feed">entrar</a> <br>
-        <a href= "#cadastre">cadastre</a> 
+        <section class="content">
+            <div class="title">
+                <div class="logo">
+                    <h1>MCMR</h1>
+                    <h3>Meu Corpo Minhas Regras</h3>
+                </div>
+                <img src="./img/logo.png">
+            </div>
+            <h2>Entre ou crie uma conta</h2>
+            <!-- DIV DO CAMPO DE ENTRAR -->
+            <div class="container">
+                <h1>Entrar</h1>
+                <a href="#google" class="link-google"><i class="fa-brands fa-google"></i> Entrar com o Google</a>
+                <a href="#feed" class="link-entrar">Entrar</a>
+            </div>
+
+            <a href= "#cadastre">Nova Conta</a>
+        </section> 
     `;
     container.innerHTML = template; 
 

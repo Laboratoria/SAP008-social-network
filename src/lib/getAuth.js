967ed90d291a68715146bc26dfaa2628523841f9
Juliene-Araujo <julienefjs@gmail.com>
2022-09-30 18:06:05 -0300

    updateprofile

--- src/lib/getAuth.js
+++ src/lib/getAuth.js
@@ -1,16 +1,28 @@
 // aqui exportaras las funciones que necesites//
 
 import { app } from "../config-firebase.js";
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.9.4/firebase-auth.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, updateProfile } from "https://www.gstatic.com/firebasejs/9.9.4/firebase-auth.js";
 const auth = getAuth(app)
 
 
-export const register = (email, password) => {
+export const register = (email, password, profileName, realName) => {
   return createUserWithEmailAndPassword(auth, email, password)
     .then((banana) => {
       // Signed in
       const user = banana.user;
       console.log(user)
+      updateProfile(auth.currentUser, {
+        displayName: profileName,
+        realName: realName 
+      }).then(() => {
+        // Profile updated!
+        console.log("funcionou")
+        // ...
+      }).catch((error) => {
+        // An error occurred
+        // ...
+      });
+      
       // ...
     })
     .then(function () {

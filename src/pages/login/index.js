a37b9eff0f2cbd08940e72928440c901280946d8
Bellalimalima <isabellalimaelima@gmail.com>
2022-10-02 19:55:59 -0300

    responsividade login e signup

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -2,61 +2,46 @@ import { loginEmailPassword, logout, signInGoogle } from '../../lib/auth.js';
 
 export default () => {
   const container = document.createElement('div');
-  const template = `
-    <div class="container">
-      <section class="login">      
-          <img src="./img/picsfem.png" class="login-logo">
-
-          <h1 class="login-title">Fazer login</h1>
-
-          <div id="form-container"></div>
-
-          <form id="login-form">
-            <div class="email-pass">
-            
-              <label class="login-label">
-                <span>Email:</span>
-                <input id="txtEmail" type="email" name="email" class="input">
-              </label>
+  const template = `    
+  <div class="container">
+    <section class="login-container">      
+      <img src="./img/picsfem.png" class="logo-login">                  
+      <form class="login-form" id="login-form">
+        <div class="email-pass">            
+          <label class="login-label" for="email">                
+            <input id="txtEmail" type="email" name="email" class="input" placeholder="Email" required>
+          </label>
               
-              <label class="login-label">
-                <span>Senha</span>
-                <input id="txtPassword" type="password" name="password" class="input">
-              </label>
+          <label class="login-label" for="password">                
+            <input id="txtPassword" type="password" name="password" class="input" placeholder="Senha" minlength="8" required>
+          </label>
             
-            </div>
+        </div>           
             
-            <div class="txt-error" id="txt-error"></div>
+        <div class="signup-forgot">              
+          <a href="#signup" type="button" class="login-link" style="text-decoration:none">Criar conta</a>
+              
+          <a href="#" class="login-link">Esqueceu a senha?</a>
+        </div>            
             
-            <div class="ads">
-              <div class="Criar">
-                <a href="#signup" id="btnSignupSpa" type="button" class="button2">Criar conta</a>
-              </div>
-
-              <a href="#" class="login-link">Esqueceu a senha?</a>
-            </div>
-
-            <span id="message" class="message"></span>
-
-            <div class="entrar">
-              <button id="btnLogin" type="button" class="button">Entrar</button>
-            </div>          
-
-            <a class="login-link2">Logar como:</a>
-
-            <div class="login-icons">
-              <button id="btn-gmail" type="button" class="icons-button">
-              <img src="./img/icongmail.png" alt="gmail">
-              </button>          
-            </div>
-          </form>                
-      </section>
-
-      <img class="wallpaper" src="./img/foto.png">       
-
-    </div>
-    <button id="btnLogout" type="button">Log out</button>
-     
+        <button id="btnLogin" type="button" class="btn-login">Entrar</button>                    
+            
+        <p class="txt-error" id="txt-error"></p>                    
+            
+      </form>
+      <div class="line-container">
+        <hr class="line">
+          <span class="txt-line">ou</span>
+        <hr class="line">            
+      </div>
+            
+      <button id="btn-gmail" type="button" class="icon-button">Continue com
+        <img class="google-icon" src="./img/google-icon.png" alt="google-icon">
+      </button>
+      <button id="btnLogout" type="button">Log out</button>
+    </section>
+    <img class="wallpaper-login" src="./img/foto.png">  
+  </div>     
     `;
 
   container.innerHTML = template;
--- src/pages/signup/index.js
+++ src/pages/signup/index.js
@@ -4,22 +4,27 @@ export default function signUp() {
   const container = document.createElement('div');
 
   const template = `
-    <form>
-      <label class= "name"  for="name">Nome:
-        <input class="input" type="text" required>
-      </label>
-
-      <label class= "email" for="email">Email:
-        <input class="input" type="email" id="txtEmail" required>
-      </label>
-      
-      <label class= "password" for="password">Senha:
-        <input class="input" type="password" id="txtPassword" minlength="8" required>
-      </label>
-      
-      <a href="#login" id="btnSignup" type="button" class="buttonSignup">CRIAR CONTA</a>
-        
-    </form>
+    <div class="container">
+     <section class="signup-container">      
+        <img src="./img/picsfem.png" class="logo-signup">              
+        <form class="signup-form" >
+          <div class="signup-box">
+            <label class= "signup-label" for="name">              
+              <input class="input-signup" id="txtName" type="text" name="name" placeholder="Nome" required>
+            </label>            
+            <label class= "signup-label" for="email">              
+              <input class="input-signup" id="txtEmail" type="email" name="email" id="txtEmail" placeholder="Email" required>
+            </label>
+          
+            <label class= "signup-label" for="password">              
+              <input class="input-signup" id="txtPassword" type="password" name="password" id="txtPassword" minlength="8" placeholder="Senha" required>
+            </label>
+          </div>
+          <a href="#login" id="btnSignup" type="button" class="btnSignup" style="text-decoration:none">CRIAR CONTA</a>
+        </form>                
+     </section>
+     <img class="wallpaper-signup" src="./img/foto.png">       
+    </div>    
     `;
   container.innerHTML = template;
 

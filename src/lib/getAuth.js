7a3da24ab32e75ad5948bd4b133b52e8f856045b
andressaolive <andressa_aoliveira@yahoo.com.br>
2022-10-03 14:06:22 -0300

    refatoração login

--- src/lib/getAuth.js
+++ src/lib/getAuth.js
@@ -1,5 +1,3 @@
-// aqui exportaras las funciones que necesites//
-
 import { app } from "../config-firebase.js";
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.9.4/firebase-auth.js";
 const auth = getAuth(app)
@@ -7,11 +5,9 @@ const auth = getAuth(app)
 
 export const register = (email, password) => {
   return createUserWithEmailAndPassword(auth, email, password)
-    .then((banana) => {
-      // Signed in
-      const user = banana.user;
+    .then((userCredencial) => {
+      const user = userCredencial.user;
       console.log(user)
-      // ...
     })
     .then(function () {
       alert("Bem vindo" + email.value);
@@ -34,7 +30,7 @@ export const signIn = (email, password) => {
     })
     .catch(function (error) {
       console.error(error.code)
-      alert("falhou")
+      alert("e-mail ou senha inválido")
 
     });
 

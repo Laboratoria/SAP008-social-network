e44d771910398e2957bcf9e04e422ec457be5cc9
Amanda Holanda <amandaholanda_@hotmail.com>
2022-10-03 14:35:23 -0300

    responsividade pos code review

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -5,44 +5,46 @@ export default () => {
   container.classList.add('wrapper-login');
   const template = `    
     <section class="container-login"> 
-      <div class="container-login-1"></div>
-      <div class="container-login-2"></div>       
-      <img src="./img/picsfem.png" class="logo-login">                  
+      <div class="container-login1">
+        <img src="./img/picsfem.png" class="logo-login">                  
 
-      <form class="form-login">
-        <div class="email-and-password-container">            
-          <label class="login-label" for="email">                
-            <input id="txtEmail" type="email" name="email" class="input-login" placeholder="Email" required>
-          </label>
+        <form class="form-login">
+          <div class="email-and-password-container">            
+            <label class="login-label" for="email">                
+              <input id="txtEmail" type="email" name="email" class="input-login" placeholder="Email" required>
+            </label>
+                
+            <label class="login-label" for="password">                
+              <input id="txtPassword" type="password" name="password" class="input-login" placeholder="Senha" minlength="8" required>
+            </label>
               
-          <label class="login-label" for="password">                
-            <input id="txtPassword" type="password" name="password" class="input-login" placeholder="Senha" minlength="8" required>
-          </label>
-            
-        </div>           
-            
-        <div class="signup-and-forgotpassword-container">              
-          <a href="#signup" type="button" class="signup-and-forgotpassword" style="text-decoration:none">Criar conta</a>
+          </div>           
               
-          <a href="#" class="signup-and-forgotpassword">Esqueceu a senha?</a>
-        </div>   
+          <div class="signup-and-forgotpassword-container">              
+            <a href="#signup" type="button" class="signup-and-forgotpassword" style="text-decoration:none">Criar conta</a>
+                
+            <a href="#" class="signup-and-forgotpassword">Esqueceu a senha?</a>
+          </div>   
 
-        <p class="txt-error" id="txt-error"></p>   
+          <p class="txt-error" id="txt-error"></p>   
 
-        <button id="btnLogin" type="button" class="btn-login">Entrar</button>                   
-                                                 
-      </form>
-
-      <div class="line-container">
-        <hr class="line">
-          <span class="txt-line">ou</span>
-        <hr class="line">            
+          <button id="btnLogin" type="button" class="btn-login">Entrar</button>                   
+                                                  
+        </form>
       </div>
-            
-      <button id="btn-gmail" type="button" class="btn-google">
-        <img class="google-icon" src="./img/google-icon.png" alt="google-icon">
-        Continue com google
-      </button>            
+
+      <div class="container-login2">
+        <div class="line-container">
+          <hr class="line">
+            <span class="txt-line">ou</span>
+          <hr class="line">            
+        </div>
+              
+        <button id="btn-gmail" type="button" class="btn-google">
+          <img class="google-icon" src="./img/google-icon.png" alt="google-icon">
+          Continue com google
+        </button>
+      </div>               
       
     </section>       
 

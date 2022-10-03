c626d95ed8882ad4187c0491d7705556402bd1f6
Juhreisf <109034395+Juhreisf@users.noreply.github.com>
2022-10-03 14:26:20 -0300

    arquivos js

---
+++ src/config-firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-app.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC3bxCqhfcT9hZYxdSg-waQ6Mb9FMnM-TI",
+  authDomain: "cookie-8fdf2.firebaseapp.com",
+  projectId: "cookie-8fdf2",
+  storageBucket: "cookie-8fdf2.appspot.com",
+  messagingSenderId: "757632763950",
+  appId: "1:757632763950:web:d6bb2c2cd9416102a9b7bf"
+};
+
+const app = initializeApp(firebaseConfig);

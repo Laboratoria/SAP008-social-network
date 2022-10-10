b8825cbb3cdca68d294a73ab0a8516a706813988
Gleyci-Macena <gleycianemc@gmail.com>
2022-10-10 13:39:27 -0300

    icons

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -9,15 +9,19 @@ export default () => {
         <img src="./img/picsfem.png" class="logo-login">                  
 
         <form class="form-login">
-          <div class="email-and-password-container">            
-            <label class="login-label" id="loginLabel" for="email">                
+          <div class="email-and-password-container">
+            <div class="iconmail"> 
+            <img class="mail" src="/img/icons8-mail-48.png">      
+            <label class="login-label" id="loginLabel" for="email">              
               <input id="txtEmail" type="email" name="email" class="input-login" placeholder="Email" required>
             </label>
-                
+            </div> 
+            <div class="iconmail"> 
+            <img class="mail" src="/img/icons8-heart-lock-50.png">
             <label class="login-label" id="loginLabel" for="password">                
               <input id="txtPassword" type="password" name="password" class="input-login" placeholder="Senha" minlength="8" required>
             </label>              
-
+            </div>
           </div>           
               
           <div class="signup-and-forgotpassword-container">              

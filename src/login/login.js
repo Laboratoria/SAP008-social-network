1275a0be406522b0a8457caee7ad3e18ea4fa919
Andrea dos Santos <contato.andreadossantos@gmail.com>
2022-10-10 17:13:50 -0300

    estilizacao CSS e identacao da pagina login

--- src/login/login.js
+++ src/login/login.js
@@ -1,52 +1,51 @@
 import { userLogin, loginGoogle } from '../firebase/firebase.js' 
-import  {redirect} from "../redirect.js"                                     
+import  {redirect} from '../redirect.js'                                     
 
 export default () => {
     const container = document.createElement('div');
-    const template = ` 
-        
-                 
-        <section id="login">
-
-            <div class="image"> 
-                    <img  class="background-login" src="./images/3f33b30335ec98928fa197ddc3866963.jpg" alt="background">
+    container.classList.add('wrapper-login');
+    const template = `       
+        <div class ='container-login'>
+            <div class='container-logo'>
+                <img src='./images/logo_02_blue_081E26.png' id='logo-login'alt='logo do título'>
             </div>
-
-            <div class="container-login">
-                <img  class="logo-blue-login" src="./images/logo_02_blue_081E26.png" alt="logo do título">
-                <h1 class="sub-title">MOMENTOS MUSICAIS INESQUECÍVEIS</h1> 
-                <form> 
-                    <div>
-                        <input class="box-name" type="e-mail" id="e-mail" placeholder="Digite seu e-mail"/>
-                        <span class="material-icons">mail_outline</span>
-                        
-                    </ div> 
-                    <input class="box-password" type="password" id="senha" placeholder="Digite sua senha"/> 
-                    <span class="material-icons">lock_outline</span>
-                    <input class="box-enter" type="submit" id="btn-submit" value="Entrar">
-                    <button class="new-password" type="button" id="btn-password"><a href = "#password">Esqueci a senha</button>
-                    <input class="box-login-google" type="button" id="btn-google" value="Login com Google"><img class="logo-google" src="./images/simbolo-do-google.png">
-                    <button class="box-register" type="button" id="btn-register" ><a href = "#register">Cadastre-se</button>     
-                </form>
-            </div>    
-            <footer>
-            <p class="footer-text">Desenvolvido por
-            <a href="https://github.com/AngelMelo12">
-            <span class="name"> Angelica Melo, </span>
-            </a>
-
-            <a href="https://github.com/Canzua">
-            <span class="name"> Andrea dos Santos e </span>
-            </a>
+            <div class='container-subtitle'
+                <h1 id="sub-title">MOMENTOS MUSICAIS INESQUECÍVEIS</h1>
+            </div>
+           
+            <form class='container-form-login'> 
+                <div class='container-email-password>            
+                    <input class='box-email' id='email-user' type='email' placeholder='Digite seu e-mail'/>
+                    <span class='icon-mail'>mail_outline</span>                         
+                    <input class='box-password 'id='password-user' type='password' placeholder='Digite sua senha'/>
+                    <span class='icon-password'>lock_outline</span>  
+                </ div> 
+                <div class='container-buttons'>
+                    <button class='box-enter'  type='submit' id='btn-submit'>Entrar</button>
+                    <button class='new-password' type='button'><a href = '#password' id='btn-password'>Esqueci a senha</button>
+                    <input class='box-login-google' type='button' id='btn-google' value='Login com Google'><img class='logo-google' src='./images/simbolo-do-google.png'>
+                    <button class='box-register' type='button' id='btn-register'><a href = '#register'>Cadastre-se</button> 
+                </div>
+            </form>
     
-            <a href="https://github.com/ClareanaRibeiro">
-            <span class="name"> Clareana Ribeiro </span>
-            </a>
-            </p>    
-        </footer>
-        </section> 
+            <footer>
+                <p class='footer-text'>Desenvolvido por
+                    <a href='https://github.com/AngelMelo12'>
+                        <span class='name'> Angelica Melo, </span>
+                    </a>
+                    <a href='https://github.com/Canzua'>
+                        <span class="name"> Andrea dos Santos e </span>
+                    </a>   
+                    <a href='https://github.com/ClareanaRibeiro'>
+                        <span class="name"> Clareana Ribeiro </span>
+                    </a>
+                </p>    
+            </footer>
+
+        </div> 
    
     `;
+  
 
     container.innerHTML = template;
 
@@ -57,21 +56,21 @@ export default () => {
         const password = document.querySelector('#senha').value;
         userLogin(email, password)
         .then(() => {                                          
-            redirect ("#timeline")
-          })                                                      
-          .catch((error) => {                                                            
+            redirect ('#timeline')
+        })                                                      
+        .catch((error) => {                                                            
             const errorMessage = error.message;
             window.alert(errorMessage);
-          });
+        });
     });
 
     container.querySelector('#btn-google').addEventListener('click', e => {
         e.preventDefault();
         loginGoogle();
     });
+    
 
-
-    return container;
+    return container;  
 
 
 }

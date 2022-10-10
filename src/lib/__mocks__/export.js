75ed50b6b13e8aa5a1a9229f2ac3401b098589bb
andressaolive <andressa_aoliveira@yahoo.com.br>
2022-10-10 19:21:09 -0300

    teste login

--- src/lib/__mocks__/export.js
+++ src/lib/__mocks__/export.js
@@ -1,5 +1,6 @@
-export const initializeApp = jest.fn()
-export const getAuth = jest.fn()
-export const createUserWithEmailAndPassword = jest.fn()
-export const signInWithEmailAndPassword = jest.fn()
-export const updateProfile = jest.fn()
+export const initializeApp = jest.fn();
+export const getAuth = jest.fn();
+export const createUserWithEmailAndPassword = jest.fn();
+export const signInWithEmailAndPassword = jest.fn();
+export const updateProfile = jest.fn();
+export const signInGoogle = jest.fn()
--- test/lib/auth.spec.js
+++ test/lib/auth.spec.js
@@ -1,4 +1,13 @@
-import {register} from "../../src/lib/auth.js"
-import {createUserWithEmailAndPassword, updateProfile} from "../../src/lib/export.js"
+import {signIn, signInGoogle} from "../../src/lib/auth.js"
+import {signInWithEmailAndPassword, signInGoogle} from "../../src/lib/export.js"
 
 jest.mock("../../src/lib/export.js")
+
+describe('signIn', () => {
+    it('a função deve ser chamada uma vez', () => {
+      const email = 'email@gmail.com';
+      const password = '123456';
+      signIn (email, password);
+      expect(signInWithEmailAndPassword).toHaveBeenCalledTimes(1);
+      });
+    });

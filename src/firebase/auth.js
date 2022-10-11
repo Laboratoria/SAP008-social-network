2e7241b42e0874edc6cae7ef2271735daa0bdd41
devgabrielasene <gabrielasjjx@gmail.com>
2022-10-10 23:11:03 -0300

    :feat: reduz função async creatUserWithEmailAndPassword pra tratar erros em outra pasta

--- src/firebase/auth.js
+++ src/firebase/auth.js
@@ -11,17 +11,9 @@ import { app } from './config.js';
 export const auth = getAuth(app);
 export const provider = new GoogleAuthProvider(app);
 
-export const newUser = async (email, password) => {
-  try {
-    const newUserLocal = await createUserWithEmailAndPassword(
-      auth,
-      email,
-      password,
-    );
+export async function newUser(email, password) {
+    const newUserLocal = await createUserWithEmailAndPassword(auth, email, password);
     return newUserLocal;
-  } catch (error) {
-    return error;
-  }
 };
 
 export function loginUser(email, password) {

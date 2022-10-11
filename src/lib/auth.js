2149c8faa8c6b8c90a7298ff468e708db0187a08
andressaolive <andressa_aoliveira@yahoo.com.br>
2022-10-11 17:42:28 -0300

    conflito

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,24 +1,15 @@
 // aqui exportaras las funciones que necesites//
 
-import { app } from "./config.js";
+import { app } from './config.js';
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, updateProfile, GoogleAuthProvider, signInWithPopup, } from "./export.js";
-const auth = getAuth(app)
+const auth = getAuth(app);
 
+export const register = (email, password, profileName) => createUserWithEmailAndPassword(auth, email, password);
+  .then(() => updateProfile(auth.currentUser, {
+    displayName: profileName,
+  }));
 
-export const register = (email, password, profileName) => {
-  return createUserWithEmailAndPassword(auth, email, password)
-    .then(() => {
-      return updateProfile(auth.currentUser, {
-        displayName: profileName,
-      })
-    })
-
-}
-
-
-export const signIn = (email, password) => {
-  return signInWithEmailAndPassword(auth, email, password)
-}
+export const signIn = (email, password) => signInWithEmailAndPassword(auth, email, password);
 
 export const signInGoogle = () => {
   const provider = new GoogleAuthProvider();

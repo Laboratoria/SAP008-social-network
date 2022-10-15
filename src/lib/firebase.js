d970bf6c4f54068a57eded60cfe0465a988aab2b
blericalopes <blericalopes@outlook.com>
2022-10-14 22:54:54 -0300

    template posts, função print posts, função pegar primeira letra do name

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,4 +1,4 @@
 export { initializeApp } from 'https://www.gstatic.com/firebasejs/9.10.0/firebase-app.js';
 export { getAuth, createUserWithEmailAndPassword, signInWithPopup, GoogleAuthProvider, signInWithEmailAndPassword, signOut, updateProfile } from 'https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js';
-export { collection, addDoc, getFirestore } from 'https://www.gstatic.com/firebasejs/9.10.0/firebase-firestore.js';
+export { collection, addDoc, getFirestore, getDocs } from 'https://www.gstatic.com/firebasejs/9.10.0/firebase-firestore.js';
 

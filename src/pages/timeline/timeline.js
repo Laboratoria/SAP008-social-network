254fb4e8cc007dbdfc1ff429a8639366bf6a8f07
Bruna Lima <brunalima2160@hotmail.com>
2022-10-17 16:37:18 -0400

    :style: estilização da tela feed mobile e algumas alterações no arquivo timeline.js

--- src/pages/timeline/timeline.js
+++ src/pages/timeline/timeline.js
@@ -8,15 +8,28 @@ export default () => {
 	  <img class='logo-timeline' src='./imagens/logo-mobile.png' alt='logo'>
 	</figure>
 	 <header>
-	    <button id='logout-btn'><img class='logout-icon' src='./imagens/logout.svg' alt='signout-icon'></button>
-	 <header>
-    <main id='create-post' class='page-home'>
-        <div id='icon-div'>
-            <img src='' alt='' class='' class='user-icon-post'>
-        </div>
-        <textarea placeholder='Compartilhe sua vivência...' id='post-publish' cols='60' rows='10' style='resize:none' maxlength='200'></textarea>
-         <button id='add-image'><img class='add-photo' src='./imagens/icon-photo.svg' alt='add-image'></button>
-        <button id='publish-btn'><img class='btn-post' src='./imagens/btn-post.svg' alt='add-image'></button>
+	    <button id='logout-btn' class='logout-btn'><img class='logout-icon' src='./imagens/logout.svg' alt='signout-icon'></button>
+
+      <h2 class='display-name'>Olá, </h2>
+	 </header>
+    <main id='create-post' class='container-main'>
+ 
+      <section class='container-post'>
+        <figure>
+          <img class='img-profile' id='img-profile' />
+        </figure>
+
+        <textarea class='post-publish' id='post-publish' placeholder='Compartilhe sua vivência...' cols='60' rows='10' style='resize:none'></textarea>
+      </section>
+        
+      <div class='linha-um'></div>
+      <section class='container-btn'>
+        <button class='btn-add-img' id='add-image'><img class='btn-add-img-icon' src='./imagens/icon-photo.svg' alt='add-image'></button>
+
+        <button class='publish-btn' id='publish-btn'><img class='publish-post-icon' src='./imagens/btn-post.svg' alt='add-image'></button>
+      </section>
+      <div class='linha-dois'></div>
+
     	<section id='post-timeline'></section>
 	</main>
   `;
@@ -24,12 +37,15 @@ export default () => {
   container.innerHTML = template;
 
   const btnLogout = container.querySelector('#logout-btn');
+  const displayName = container.querySelector('.display-name');
 
   btnLogout.addEventListener('click', (e) => {
     e.preventDefault();
     logout();
     window.location.hash = '#login';
   });
+
+  
 
   return container;
 };

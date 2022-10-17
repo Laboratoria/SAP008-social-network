674c7e41cce4d95fefa0330d8af9b20e3f2e6445
thamisantos <tamimartins31113@gmail.com>
2022-10-17 18:54:57 -0300

    add hash location

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,16 +1,16 @@
 import {
-  getAuth, // autenticação
+  getAuth,
   // createUserWithEmailAndPassword, // criar usuário
-  signInWithEmailAndPassword, // fazer login
+  signInWithEmailAndPassword,
   // signOut, // sair da conta
-  GoogleAuthProvider, // entrar com o Google
+  GoogleAuthProvider,
   signInWithPopup,
   // eslint-disable-next-line import/no-unresolved
 } from 'https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js';
 
 import { app } from '../firebase.js';
 
-const auth = getAuth(app);
-const provider = new GoogleAuthProvider(app);
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider(app);
 export const userLogin = (email, password) => signInWithEmailAndPassword(auth, email, password);
 export const loginGoogle = () => signInWithPopup(auth, provider);
--- src/main.js
+++ src/main.js
@@ -1,26 +1,26 @@
-import cadastro from './pages/cadastro/cadastro.js';
+// import cadastro from './pages/cadastro/cadastro.js';
 
 import login from './pages/login/login.js';
 
 const main = document.querySelector('#root');
 
-const verify = () => {
-  main.innerHTML = '';
-  switch (window.location.hash) {
-    case '':
-      main.appendChild(login());
-      break;
-    case '#register':
-      main.appendChild(cadastro());
-      break;
-    default:
-      main.appendChild(login());
-  }
-};
+// const verify = () => {
+//   main.innerHTML = '';
+//   switch (window.location.hash) {
+//     case '':
+//       main.appendChild(login());
+//       break;
+//     // case '#register':
+//     //   main.appendChild(cadastro());
+//     //   break;
+//     default:
+//       main.appendChild(login());
+//   }
+// };
 
-window.addEventListener('hashchange', verify);
+// window.addEventListener('hashchange', verify);
 
 window.addEventListener('load', () => {
-  // main.appendChild(login());
-  verify();
+  main.appendChild(login());
+  // verify();
 });

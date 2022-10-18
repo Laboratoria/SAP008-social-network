5e6d7d82efd3f72b5b8f8e877baf69ccd3a43fb6
andradeannac <andradeannac@gmail.com>
2022-10-18 00:31:08 -0300

    atualizações css register

--- src/Templates/register.js
+++ src/Templates/register.js
@@ -4,31 +4,38 @@ export default () => {
   const container = document.createElement('div');
 
   const template = `
-    <section class="register">
-        <img src="return.png" alt="voltar" id="voltarRegister">
-
-        <h1 class='tituloRegister'>Bem vinda!</h1>
-        <img src="cookie.png" alt="logo-cookie" id="cookieRegister">
-
-
+      <header class="navRegister">
+        <div class='divBtnReturn'>
+          <img src="return.png" alt="voltar" id="voltarRegister">
+        </div>
+        <div>
+          <h1 class='tituloRegister'>Bem vinda!</h1>
+        </div>
+        <div>
+          <img src="cookie.png" alt="logo-cookie" id="cookieRegister">
+        </div>
+      </header>
+      <main class="register">
         <form class="formRegister">
-
-            <label>Nome completo</label>
-            <input type="text" id="nameRegister" >
-
-            <label>Data de nascimento</label>
-            <input type="date" id="dateRegister" >
-
-            <label>Email</label>
-            <input type="email"  id="usernameRegister" placeholder="example@gmail.com">
-
-            <label>Senha</label>
-            <input type="password"  id="passwordRegister" placeholder="********">
-
-            <button id="CadastrarRegister">Cadastre-se</button>  
+          <div class="boxLegendaInput">
+            <label class="legendaRegister">Nome completo</label>
+            <input type="text" id="nameRegister" placeholder="digite seu nome" class="btnRegister" >
+          </div>
+          <div class="boxLegendaInput">
+            <label class="legendaRegister">Data de nascimento</label>
+            <input type="date" id="dateRegister" class="btnRegister">
+          </div>
+          <div class="boxLegendaInput">
+            <label class="legendaRegister">Email</label>
+            <input type="email"  id="usernameRegister" placeholder="example@gmail.com"  class="btnRegister">
+          </div>
+          <div class="boxLegendaInput">
+            <label class="legendaRegister">Senha</label>
+            <input type="password"  id="passwordRegister" placeholder="********"  class="btnRegister">
+          </div>
+            <button id="cadastrarRegister">Cadastre-se</button>  
         </form>
-     
-    </section>
+        </main>
         `;
   container.innerHTML = template;
   const form = container.querySelector('.formRegister');

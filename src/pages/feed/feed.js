36daf0bbb2b011ab90ccc0cea3b566f4bbd02ca9
andresavieira28 <andresacg_14@hotmail.com>
2022-10-18 16:37:35 -0300

    Iniciando função de criar poste

--- src/pages/feed/feed.js
+++ src/pages/feed/feed.js
@@ -1,22 +1,37 @@
 /* eslint-disable no-console */
+
 import { logOutUser } from '../../lib/auth.js';
 
 export default () => {
   const sectionFeed = document.createElement('div');
   const contentFeed = `
     <div>
-      <header>
-          <img src='' class='header' alt='header'>
-          <img src='/src/img/logoTranp.png' class='loginho' alt='Logo Peq Wanderlust'>
-          <nav class='navbar'>
-            <input id='logOut' class='btnSIgnInOut' type='button' value='Sair'>
-          </nav>
+      <header class="headerFeed">
+          <img src='/src/img/logo.png' class='loginhoFeed' alt='Logo Peq Wanderlust'>
       </header>
-      <main>
-      </main>
+      <nav class="navBar">
+        <ul>
+          <li><a class='btnSIgnInOut' id='logOut'><img src='/src/img/btnSair.png' alt='seta para sair'</a></li>
+        </ul>
+      </nav>
+      <section class="msgBoasvindas">
+        <p> Olá, Fulana!</p>
+      </section>
+      <main clas="corpotimeline">
+        <section class="boxModelPost">
+          <form>
+            <textarea id="text-publish" class="inputText" rows="5" cols="40" placeholder="Escreva detalhes sobre a estadia em sua residência..."></textarea>
+            <button type="submit" id="publish-btn" class="publicBtn">Publicar</button>
+          </form>
+        </section>
+        <section id="post-feed">
+        </section>
+      </main>  
     </div>`;
   sectionFeed.innerHTML = contentFeed;
 
+  const textAreaPost = sectionFeed.querySelector('#text-publish');
+  const btnPublish = sectionFeed.querySelector('#publish-btn');
   const btnLogOut = sectionFeed.querySelector('#logOut');
   btnLogOut.addEventListener('click', () => {
     logOutUser()

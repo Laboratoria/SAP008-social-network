b0db214ff1c9faae655ccd5b73e10aef8f3619c0
andresavieira28 <andresacg_14@hotmail.com>
2022-10-18 10:02:35 -0300

    refatorando erros do cath

--- src/pages/password/password.js
+++ src/pages/password/password.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-console */
 import { resetPassword } from '../../lib/auth.js';
+import { errorFire } from '../../lib/errorFirebase.js';
 
 export default () => {
   const sectionPassword = document.createElement('section');
@@ -16,6 +17,12 @@ export default () => {
           <input class="inputEmail" type="text" id="userEmail" placeholder="E-mail" value="">
         </p>
         <p>
+          <div id="msgSucesso" class="msgErro"></div>
+        </p>
+        <p>
+          <div id='erro-Firebase' class='msgErro'></div>
+        </p>
+        <p>
           <input id="btn-reset" class="ResetPassword" type="button" value="Enviar email">
         </p>
         <p class="btnBack">
@@ -26,14 +33,17 @@ export default () => {
   sectionPassword.innerHTML = content;
   const inputEmail = sectionPassword.querySelector('#userEmail');
   const btnResetPassword = sectionPassword.querySelector('#btn-reset');
+  const msgSucsses = sectionPassword.querySelector('#msgSucesso');
+  const msgFirePassword = sectionPassword.querySelector('#erro-Firebase');
   btnResetPassword.addEventListener('click', (e) => {
     e.preventDefault();
     resetPassword(inputEmail.value)
       .then(() => {
-        console.log('Email enviado com sucesso');
+        msgSucsses.innerHTML = 'Email enviado com sucesso!';
       })
       .cath((error) => {
-        console.error(error);
+        const errorCode = errorFire(error.code);
+        msgFirePassword.innerHTML = errorCode;
       });
   });
   return sectionPassword;

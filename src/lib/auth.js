881ef3087f363bbc940eed53b33219a74b790db8
andresavieira28 <andresacg_14@hotmail.com>
2022-10-19 13:08:54 -0300

    resolvendo conflito de merge

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,9 +1,6 @@
-/* eslint-disable arrow-body-style */
-/* eslint-disable no-trailing-spaces */
 /* eslint-disable no-alert */
 /* eslint-disable no-console */
-import { async } from 'regenerator-runtime';
-import { app, db } from './configuration.js';
+import { app } from './configuration.js';
 
 import {
   getAuth,
@@ -15,9 +12,6 @@ import {
   createUserWithEmailAndPassword,
   updateProfile,
   sendPasswordResetEmail,
-  collection,
-  getDocs,
-  addDoc,
 } from './firebase.js';
 
 const auth = getAuth(app);
@@ -43,37 +37,6 @@ const updateDisplayName = (user, displayName) => updateProfile(user, { displayNa
 
 const resetPassword = (email) => sendPasswordResetEmail(auth, email);
 
-const nomeUsuario = () => auth.currentUser.displayName;
-
-const idUsuario = () => auth.currentUser.uid;
-
-const criarPost = async (textPost) => {
-  try {
-    const docRef = await addDoc(collection(db, 'posts'), {
-      name: auth.currentUser.displayName,
-      author: auth.currentUser.uid,
-      texto: textPost,
-      like: [],
-    });
-  } catch (e) {
-    console.error("Error adding document: ", e);
-  }
-
-  /*criar um map aqui ao inves do foreach e push*/
-
-  const postsTela = async () => {
-    try {
-      const querySnapshot = await getDocs(collection(db, "posts"));
-      const arrayVazio = [];
-      querySnapshot.forEach((posts) => {
-        arrayVazio.push({ ...posts.data(), id: posts.id });
-      });
-      console.log(postArray);
-    } catch (e) {
-    console.log(e)
-  }
-};
-
 export {
   logInUser,
   signInWithGoogle,
@@ -81,7 +44,4 @@ export {
   createRegister,
   updateDisplayName,
   resetPassword,
-  criarPost,
-  nomeUsuario,
-  idUsuario,
 };

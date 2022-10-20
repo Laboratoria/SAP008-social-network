46a23ab2d3781d90c9bb15a30c313c33254352f2
Barbara <scatolinbarbara@gmail.com>
2022-10-20 16:54:03 -0300

    alteracaoCssRegister

--- src/Templates/register.js
+++ src/Templates/register.js
@@ -4,38 +4,42 @@ export default () => {
   const container = document.createElement('div');
 
   const template = `
-
-      <header class="navRegister">
-        <div class='retorno-titulo-logo'>
+    <section class='contenderRegister'section>
+      <div class="navRegister">
+  
           <img src="img/return.png" alt="voltar" id="voltarRegister">
         
           <h1 class='tituloRegister'>Bem vinda!</h1>
-       
-          <img src="img/cookie.png" alt="logo-cookie" id="cookieRegister">
-        </div>
-      </header>
+        
+      </div>
+
       <main class="register">
         <form class="formRegister">
+
           <div class="boxLegendaInput">
             <label class="legendaRegister">Nome completo</label>
             <input type="text" id="caixaRegister" placeholder="digite seu nome" class="btnRegister" >
           </div>
+
           <div class="boxLegendaInput">
             <label class="legendaRegister">Data de nascimento</label>
             <input type="date" id="caixaRegister" class="btnRegister">
           </div>
+
           <div class="boxLegendaInput">
             <label class="legendaRegister">Email</label>
             <input type="email"  id="caixaRegister" placeholder="example@gmail.com"  class="btnRegister">
           </div>
+
           <div class="boxLegendaInput">
             <label class="legendaRegister">Senha</label>
             <input type="password"  id="caixaRegister" placeholder="********"  class="btnRegister">
           </div>
-            <button id="cadastrarRegister">Cadastre-se</button>  
+          <button id="cadastrarRegister">Cadastre-se</button>  
 
         </form>
         </main>
+    </section>
         `;
   container.innerHTML = template;
   const form = container.querySelector('.formRegister');

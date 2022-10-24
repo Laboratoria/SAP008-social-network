62c1d2a805bb1385e970f63d629b36f69e48e8fb
deboramds <deborah.mart@hotmail.com>
2022-10-24 15:01:30 -0300

    SPA

--- src/main.js
+++ src/main.js
@@ -1,5 +1,14 @@
-// Este es el punto de entrada de tu aplicacion
 
-import { myFunction } from './lib/index.js';
 
-myFunction();
+const main = document.querySelector('#root');
+
+function init (){
+  window.addEventListener("hashchange", ()=>{
+    console.log("hashchange ", location.hash)
+  })
+
+}
+
+window.addEventListener("load", () => {
+   init()
+})

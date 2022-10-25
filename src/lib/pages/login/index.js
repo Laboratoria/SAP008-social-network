332851ef3f8a8f0cfdecfbe91582c67e4e211aa2
deboramds <deborah.mart@hotmail.com>
2022-10-25 19:13:27 -0300

    SPA

--- src/lib/pages/login/index.js
+++ src/lib/pages/login/index.js
@@ -1,21 +1,22 @@
 export default () => {
-    const container = document.createElement('div')
-    const template = `
-     <div class="box">
-     <h2>login</h2>
-     <form>
-     <div class="inputBox">
-     <input type= "text" name="name">
-     <label for ="">Nome</label>
+  const container = document.createElement('div')
+  const template = `
+     <div class="box">    
+      <h2>login</h2>
+      <form>
+        <div class="inputBox">
+          <input type= "text" name="name">
+          <label for ="">Nome</label>
+        </div>
+        <div class="inputBox">
+          <input type= "password" name="Password">
+          <label for ="">senha</label>
+        </div>
+       <input type= "submit" value="Entrar">
+      </form>
      </div>
-     <div class="inputBox">
-     <input type= "password" name="Password">
-     <label for ="">senha</label>
-     </div>
-     <input type= "submit" value="Entrar">
      `;
-    container.innerHTML = template;
-    return container;
-  
-  }
-  
+  container.innerHTML = template;
+  return container;
+
+}

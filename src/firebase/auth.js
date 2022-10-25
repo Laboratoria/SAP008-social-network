d3e0251c8402ef19ba8ec5a9b66353152d188c84
thamisantos <tamimartins31113@gmail.com>
2022-10-25 18:52:41 -0300

    update jest

--- src/firebase/auth.js
+++ src/firebase/auth.js
@@ -1,36 +1,27 @@
 import {
-  getFirestore,
-  addDoc,
-  collection,
   getAuth,
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
   GoogleAuthProvider,
   signInWithPopup,
+  updateProfile,
 } from './exports.js';
 
 import { app } from './config.js';
 
+export const auth = getAuth(app);
 export const provider = new GoogleAuthProvider(app);
-const auth = getAuth(app);
-
+export const userLogin = (email, password) => signInWithEmailAndPassword(auth, email, password);
+export const loginGoogle = () => signInWithPopup(auth, provider);
 export const getUserName = () => auth.currentUser.displayName;
 export const getId = () => auth.currentUser.uid;
 
-export const userLogin = (email, password) => signInWithEmailAndPassword(auth, email, password);
-
-export const loginGoogle = () => signInWithPopup(auth, provider);
-
 // eslint-disable-next-line max-len
-export const createUser = (email, password) => createUserWithEmailAndPassword(auth, email, password);
-
-// nat
-const db = getFirestore(app);
-
-async function publishPost(post) {
-  await addDoc(collection(db, 'posts'), {
-    post,
+export const createUser = (name, email, password) => createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    const user = userCredential.user;
+    console.log(user);
+    updateProfile(auth.currentUser, {
+      displayName: name,
+    });
   });
-}
-
-export { publishPost };

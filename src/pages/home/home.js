cf6e0b6e16576dc926aa66db3e0fb780fb34ad64
nunesisabela <isabelanunes.tech@gmail.com>
2022-10-27 21:12:49 -0300

    separação template string post entre currentUser e não currentUser

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,5 +1,5 @@
 import {
-  logoutUser, auth, getPosts, deletePost, forEditPost
+  logoutUser, auth, getPosts, deletePost, forEditPost,
 } from '../../lib/auth.js';
 
 // eslint-disable-next-line consistent-return
@@ -35,37 +35,61 @@ export default () => {
 
     const printPosts = async () => {
       const all = await getPosts();
-      const postsTemplate = all.map((posts) => `<div data-id="${posts.id}" class="posts">
-        <div id="text">
-          <p>@ ${posts.name}</p>
-          <p id="local" data-editlocal="${posts.id}" class="edit-local establishment" contenteditable="false">${posts.restaurant}</p>
-          <p id="adress" data-editadress="${posts.id}" class="edit-adress" contenteditable="false">${posts.adress}</p>
-          <p id="review" data-editreview="${posts.id}" class="edit-review" contenteditable="false">${posts.review}</p>
-        </div>
-
-        <button id="cancel">Cancelar</button>
-        <button id="ok" data-edit="${posts.id}">OK</button>
-    
-        <div id="modal-delete" class="hide">
-          <p>Tem certeza que deseja excluir este post?</p>
-          <button data-delete="${posts.id}" id="yes-delete">Sim</button>
-          <button id="no-close">Não</button>
-        </div>
-    
-        <aside class="infos-container">
-          <div>
-            <div id="user-image"><p class="name-letter">${firstLetter(posts.name)}</p></div>
-            <p id="grade">4.7</p>
-            <div class="icons-post">
-              <img id="heart-icon" class="icons-post-size" src="./external/svg/heart-icon.svg"/>
-              <img id="pencil-icon" class="icons-post-size icons-current-user" src="./external/svg/pencil-icon.svg"/>
-              <img id="trash-icon" class="icons-post-size icons-current-user" src="./external/svg/trash-icon.svg"/>
-            </div>
+      let postsTemplate = '';
+      const mapPosts = all.map((posts) => {
+        if (auth.currentUser === posts.author) {
+          postsTemplate = `<div data-id="${posts.id}" class="posts">
+          <div id="text">
+            <p>@ ${posts.name}</p>
+            <p id="local" data-editlocal="${posts.id}" class="edit-local establishment" contenteditable="false">${posts.restaurant}</p>
+            <p id="adress" data-editadress="${posts.id}" class="edit-adress" contenteditable="false">${posts.adress}</p>
+            <p id="review" data-editreview="${posts.id}" class="edit-review" contenteditable="false">${posts.review}</p>
           </div>
-        </aside>
-      </div>
-      <hr class="colorful-line"/>`).join('');
-      container.querySelector('.post-container').innerHTML += postsTemplate;
+  
+          <button id="cancel">Cancelar</button>
+          <button id="ok" data-edit="${posts.id}">OK</button>
+      
+          <div id="modal-delete" class="hide">
+            <p>Tem certeza que deseja excluir este post?</p>
+            <button data-delete="${posts.id}" id="yes-delete">Sim</button>
+            <button id="no-close">Não</button>
+          </div>
+      
+          <aside class="infos-container">
+            <div>
+              <div id="user-image"><p class="name-letter">${firstLetter(posts.name)}</p></div>
+              <p id="grade">4.7</p>
+              <div class="icons-post">
+                <img id="heart-icon" class="icons-post-size" src="./external/svg/heart-icon.svg"/>
+                <img id="pencil-icon" class="icons-post-size icons-current-user" src="./external/svg/pencil-icon.svg"/>
+                <img id="trash-icon" class="icons-post-size icons-current-user" src="./external/svg/trash-icon.svg"/>
+              </div>
+            </div>
+          </aside>
+        </div>
+        <hr class="colorful-line"/>`;
+          container.querySelector('.post-container').innerHTML += postsTemplate;
+        } else {
+          postsTemplate = `<div data-id="${posts.id}" class="posts">
+          <div id="text">
+            <p>@ ${posts.name}</p>
+            <p id="local" data-editlocal="${posts.id}">${posts.restaurant}</p>
+            <p id="adress" data-editadress="${posts.id}">${posts.adress}</p>
+            <p id="review" data-editreview="${posts.id}">${posts.review}</p>
+          </div>
+          <aside class="infos-container">
+            <div>
+              <div id="user-image"><p class="name-letter">${firstLetter(posts.name)}</p></div>
+              <p id="grade">4.7</p>
+              <img id="heart-icon" class="icons-post-size" src="./external/svg/heart-icon.svg"/>
+            </div>
+          </aside>
+        </div>
+        <hr class="colorful-line"/>`;
+        }
+        return postsTemplate;
+      }).join('');
+      container.querySelector('.post-container').innerHTML += mapPosts;
 
       const editPost = container.querySelector('#pencil-icon');
       const local = container.querySelector('#local');
@@ -79,25 +103,11 @@ export default () => {
       const cancelEdit = container.querySelector('#cancel');
       const okEdit = container.querySelector('#ok');
 
-
-
       function show(elemento) {
         elemento.focus();
       }
 
-      // cancelEdit.hidden = true;
-      // okEdit.hidden = true;
-      // criar função para verificar usuário com parâmetro o uid do author {if ...}
-      // if (dataBase.posts.author === auth.currentUser) { // assistir atributo data
-      //   showIcon.style.display = 'flex';
-      // }
-
-      cancelEdit.hidden = true;
-      okEdit.hidden = true;
-
       editPost.addEventListener('click', () => {
-        cancelEdit.hidden = false;
-        okEdit.hidden = false;
         review.contentEditable = true;
         show(review);
         local.contentEditable = true;
@@ -105,18 +115,14 @@ export default () => {
         adress.contentEditable = true;
         show(adress);
       });
-        
+
       okEdit.addEventListener('click', (e) => {
         const dataEditAtributte = e.target.dataset.edit;
         forEditPost(dataEditAtributte);
-        console.log(dataEditAtributte);
-        cancelEdit.hidden = true;
-        okEdit.hidden = true;
+        // console.log(dataEditAtributte);
       });
-      
+
       cancelEdit.addEventListener('click', () => {
-        cancelEdit.hidden = true;
-        okEdit.hidden = true;
         review.contentEditable = false;
         local.contentEditable = false;
         adress.contentEditable = false;
@@ -130,12 +136,14 @@ export default () => {
         modalDelete.classList.toggle('hide');
       });
 
-      yesModalDelete.addEventListener('click', (e) => {
-        const dataDeleteAtributte = e.target.dataset.delete;
-        deletePost(dataDeleteAtributte);
-        modalDelete.classList.toggle('hide');
-        const divToRemove = document.querySelector(`[data-id="${dataDeleteAtributte}]`);
-        divToRemove.remove(); // .remove() não é reconhecido no console
+      yesModalDelete.forEach((button) => {
+        button.addEventListener('click', (e) => {
+          const dataDeleteAtributte = e.target.dataset.delete;
+          deletePost(dataDeleteAtributte);
+          modalDelete.classList.toggle('hide');
+          const divToRemove = document.querySelector(`[data-id="${dataDeleteAtributte}]`);
+          divToRemove.style.display = 'none';
+        });
       });
 
       const logout = container.querySelector('#logout-icon');

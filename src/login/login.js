76d371503ff970f652ff4f7e89af3f7fd5097d7c
AngelMelo12 <angellmello1993@gmail.com>
2022-11-04 20:54:16 -0300

    ajustes de conflitos

--- src/login/login.js
+++ src/login/login.js
@@ -5,32 +5,16 @@ export default () => {
   const container = document.createElement('div');
   container.classList.add('container-login');
   const template = `
-<<<<<<< HEAD
         <div class='box-login'>
             <div class='container-top-logo'>
-=======
-  <section class = login>     
-        <div class ='box-login'>
-            <div class='container-logo'>
->>>>>>> b927c1ba1898e5d0a726e8b41fc0970ddf9609c7
                 <img src='./images/logo_02_blue_081E26.png' id='logo-login' alt='logo do título'>
             </div>
             <div class='container-top-subtitle'>
                 <p id="sub-title">MOMENTOS MUSICAIS INESQUECÍVEIS</p>
             </div>
-<<<<<<< HEAD
             <form class='form-login'>           
                     <input class='box-email' id='email-user' type='email' placeholder='Digite seu e-mail'/>                      
                     <input class='box-password' id='password-user' type='password' placeholder='Digite sua senha'/>
-=======
-            <form class='container-form-login'> 
-            
-                <div class='container-email-password>            
-                    <input class='box-email' id='email-user' type='email' placeholder='Digite seu e-mail'/>
-                        <span class='material-icons' id='icon-mail'>mail_outline</span>                         
-                    <input class='box-password' id='password-user' type='password' placeholder='Digite sua senha'/>
-                        <span class='material-icons' id='icon-password'>lock_outline</span>
->>>>>>> b927c1ba1898e5d0a726e8b41fc0970ddf9609c7
                     <button class='btn-enter' type='submit' id='btn-submit'>Entrar</button>
                     <button class='new-password' type='button'><a href='#password' id='btn-password'>Esqueci a senha</button>  
                     <button class='box-login-google' type='button' id='btn-google'><img class='logo-google' src='./images/simbolo-do-google.png'/>Login com Google</button>

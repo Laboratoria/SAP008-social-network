edca756e7aed8082dbca8e3a50d9b17603540a0b
samarijo <sabrinamariano1994@hotmail.com>
2022-11-07 11:28:10 -0300

    hashchange da pagina de register ok

--- src/main.js
+++ src/main.js
@@ -5,9 +5,20 @@
 // myFunction();
 
 import { loginElements } from './pages/login/login.js';
-
+import { mainRegister } from './pages/cadastro/cad.js';
 const root = document.querySelector('#root');
+
+const init = () => {
+  window.addEventListener('hashchange', () => {
+    root.innerHTML = '';
+    switch (window.location.hash) {
+      case '#register':
+        root.appendChild(mainRegister());
+    }
+  });
+};
 
 window.addEventListener('load', () => {
   root.appendChild(loginElements());
+  init();
 });
